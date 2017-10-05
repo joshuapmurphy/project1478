f4b928cd47e94d7a1ab3fff2a026f74dcc0fd016
Joshua Murphy <joshuapmurphy@email.arizona.edu>
2017-10-05 16:16:34 -0700

    Started the developement of Channel

--- src/project1478/Channel.java
+++ src/project1478/Channel.java
@@ -1,8 +1,55 @@
 package project1478;
 //The channel is made up of several slots. Since we simulate for 10 seconds, this is equivalent to 10s/20us = 500,000 slots.
+//This will also house the main method for the simulation since all of the processing should happen here anyway.
 public class Channel {
-	public static int NUM_SLOTS = 500000;
-	Slot[] slotChannel = new Slot[500000];
+	public static final int NUM_SLOTS = 500000;
+	private Slot[] slotChannel;
+	private int currSlotIndex;
+	private DataSource input;
+	private Transmitter tx1;
+	private Transmitter tx2;
+	private Receiver rx1;
+	private Receiver rx2;
 	
+	public Channel(){
+		slotChannel = new Slot[NUM_SLOTS];
+		currSlotIndex = 0;
+		input = new DataSource();
+		tx1 = new Transmitter();
+		tx2 = new Transmitter();
+		rx1 = new Receiver();
+		rx2 = new Receiver();
+	}
 	
+	public boolean processSlot(){
+		/*Here is the main bread and butter of the program. This function is the main part
+		 * of the program that gets called 500,000 times, once for each slot.
+		 * I'm guessing it will be a big if/else block that does different things
+		 * based off of what it was doing previously
+		*/
+		currSlotIndex++;
+		return false;
+	}
+	public int getCurrSlotIndex(){
+		return currSlotIndex;
+	}
+	public boolean outputResults(String filename){
+		//Here we will output the results either to a text file to use in matlab
+		//or just using a Java GUI.
+		//Haven't decided which will be easier.
+		
+		return false;
+	}
+	
+	public static void main(String[] args){
+		final int numberOfSlots = 500000;
+		String filename = args[0];
+		Channel network = new Channel();
+		
+		for (int i = 0; i < numberOfSlots; i++){
+			network.processSlot();
+		}
+		network.outputResults(filename);
+	}
 }
+
--- src/project1478/DataSource.java
+++ src/project1478/DataSource.java
@@ -1,5 +1,15 @@
 package project1478;
+
+import java.util.ArrayList;
+
 //This class will generate the times for frames to arrive at the transmitters for transmission.
 public class DataSource {
-
+	//may need some more instance variables here
+	private ArrayList<Integer> slotNumbers;
+	
+	//This constructor should do all of the calculations for times that frames arrive right at the start of the program
+	//In main(), we will transfer each item from this list into the transmitter queues at the appropriate times.
+	public DataSource(){
+		//Generate the times for frames to show up and convert those to slot numbers and add them to the arraylist.
+	}
 }
